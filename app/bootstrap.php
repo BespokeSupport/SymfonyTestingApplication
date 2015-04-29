bcee65aae4c6873e5bd00d092a1a51310cd29a53
Richard Seymour <web@bespoke.support>
2015-04-29 23:38:28 +0100

    testing internally

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,12 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require_once __DIR__ . '/../../vendor/autoload.php';
+if (file_exists(dirname(__FILE__) . '/../../../../vendor/autoload.php')) {
+    $loader = require_once dirname(__FILE__) . '/../../../../vendor/autoload.php';
+} elseif (file_exists(dirname(__FILE__) . '/../vendor/autoload.php')) {
+    $loader = require_once dirname(__FILE__) . '/../vendor/autoload.php';
+} else {
+    throw new \Exception('Composer Autoload not found');
+}
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
